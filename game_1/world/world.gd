ac3427aef7ee8eb2ee992b975e47e8c11642eb27
jwund <jwunderlich1127@gmail.com>
2026-07-25 05:02:28 -0500

    initial level
    added prototype beginning area
    can switch from level to test env with ;

---
+++ game_1/world/world.gd
@@ -0,0 +1,26 @@
+extends StaticBody3D
+
+const TEST = preload("res://world/test_environment.tscn")
+const LEVEL = preload("res://world/level.tscn")
+
+var current_setting: String = "test"
+
+func _ready() -> void:
+	# create the test environment and add it to the tree
+	var new_map := TEST.instantiate()
+	add_child(new_map)
+
+func _process(_delta: float) -> void:
+	# change_env bound to ; currently
+	if Input.is_action_just_pressed("change_env"):
+		# remove existing maps in the tree
+		get_tree().call_group("Maps", "queue_free")
+		# switch active map
+		if current_setting == "level":
+			var new_map := TEST.instantiate()
+			add_child(new_map)
+			current_setting = "test"
+		else:
+			var new_map := LEVEL.instantiate()
+			add_child(new_map)
+			current_setting = "level"
